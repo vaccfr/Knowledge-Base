e8e00abfd5c78fdf2d88cce0da04294060961ab2
FrenchGap <peter.pare.new@gmail.com>
2020-10-03 22:33:42 +0200

    Profile Edit page mods

--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -3,30 +3,32 @@
     <p class="mb-none text-warn">{{ trans('settings.users_system_public') }}</p>
 @endif
 
-<div class="pt-m">
-    <label class="setting-list-label">{{ trans('settings.users_details') }}</label>
-    @if($authMethod === 'standard')
-        <p class="small">{{ trans('settings.users_details_desc') }}</p>
-    @endif
-    @if($authMethod === 'ldap' || $authMethod === 'system')
-        <p class="small">{{ trans('settings.users_details_desc_no_email') }}</p>
-    @endif
-    <div class="grid half mt-m gap-xl">
-        <div>
-            <label for="name">{{ trans('auth.name') }}</label>
-            @include('form.text', ['name' => 'name'])
-        </div>
-        <div>
-            @if($authMethod !== 'ldap' || userCan('users-manage'))
-                <label for="email">{{ trans('auth.email') }}</label>
-                @include('form.text', ['name' => 'email', 'disabled' => !userCan('users-manage')])
-            @endif
+@if(userCan('users-manage'))
+    <div class="pt-m">
+        <label class="setting-list-label">{{ trans('settings.users_details') }}</label>
+        @if($authMethod === 'standard')
+            <p class="small">{{ trans('settings.users_details_desc') }}</p>
+        @endif
+        @if($authMethod === 'ldap' || $authMethod === 'system')
+            <p class="small">{{ trans('settings.users_details_desc_no_email') }}</p>
+        @endif
+        <div class="grid half mt-m gap-xl">
+            <div>
+                <label for="name">{{ trans('auth.name') }}</label>
+                @include('form.text', ['name' => 'name'])
+            </div>
+            <div>
+                @if($authMethod !== 'ldap' || userCan('users-manage'))
+                    <label for="email">{{ trans('auth.email') }}</label>
+                    @include('form.text', ['name' => 'email', 'disabled' => !userCan('users-manage')])
+                @endif
+            </div>
         </div>
     </div>
-</div>
+@endif
 
 @if(($authMethod === 'ldap' || $authMethod === 'saml2') && userCan('users-manage'))
-    <div class="grid half gap-xl v-center">
+    {{-- <div class="grid half gap-xl v-center">
         <div>
             <label class="setting-list-label">{{ trans('settings.users_external_auth_id') }}</label>
             <p class="small">{{ trans('settings.users_external_auth_id_desc') }}</p>
@@ -34,7 +36,7 @@
         <div>
             @include('form.text', ['name' => 'external_auth_id'])
         </div>
-    </div>
+    </div> --}}
 @endif
 
 @if(userCan('users-manage'))
@@ -48,7 +50,7 @@
 @endif
 
 @if($authMethod === 'standard')
-    <div new-user-password>
+    {{-- <div new-user-password>
         <label class="setting-list-label">{{ trans('settings.users_password') }}</label>
 
         @if(!isset($model))
@@ -83,5 +85,5 @@
             </div>
         </div>
 
-    </div>
+    </div> --}}
 @endif
