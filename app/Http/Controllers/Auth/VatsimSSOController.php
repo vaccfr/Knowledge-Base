98a3502a4731efedb2c7ea026a3037eb3ebdc5eb
FrenchGap <peter.pare.new@gmail.com>
2020-10-04 14:07:27 +0200

    Trial again

--- app/Http/Controllers/Auth/VatsimSSOController.php
+++ app/Http/Controllers/Auth/VatsimSSOController.php
@@ -9,6 +9,7 @@ use GuzzleHttp\Exception\ClientException;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class VatsimSSOController extends Controller
 {
